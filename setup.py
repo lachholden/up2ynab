1632289d788fb90a3c6bd79a10c9764a2c1e7f30
Lachlan Holden <lachlan.holden@gmail.com>
2020-08-16 11:03:33 +0930

    Add a setup.py to generate the CLI entrypoint

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='up2ynab',
+    version='0.0.1-dev',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        up2ynab=up2ynab:cli
+    ''',
+)
