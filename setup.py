29601e9d9b90019b494a46c5ea4b425f9a8720a4
Lachlan Holden <lachlan.holden@gmail.com>
2020-08-17 22:44:46 +0930

    Update setup.py and add MIT license

--- setup.py
+++ setup.py
@@ -1,11 +1,38 @@
 from setuptools import setup, find_packages
 
+# Read the README.md file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="up2ynab",
-    version="0.0.1-dev",
+    version="0.0.1.dev0",
+    description="CLI for synchronising the Up neobank with YNAB",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lachholden/up2ynab",
+    author="Lachlan H",
+    author_email="lachlan.holden@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Utilities",
+    ],
+    keywords="cli bank import sync YNAB Up",
+    python_requires=">=3",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["Click",],
+    install_requires=["click", "requests"],
     entry_points="""
         [console_scripts]
         up2ynab=up2ynab:cli
